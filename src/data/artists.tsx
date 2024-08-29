1649cbac0b121446e0c28bbe522f63d6f4827607
giorgio-villani <user01@DESKTOP-UJDVA60.>
2024-08-29 14:08:12 -0500

    added artist

--- src/data/artists.tsx
+++ src/data/artists.tsx
@@ -23,13 +23,13 @@ const artists = [
       'Giorgio is a Houston-based Italian American mixed media artist whose work focuses on the interplay between technology and organic elements. His installations, often involving TVs, electrical components, and animatronic sculptures, explore themes of modern-day alienation caused by technological advancements and the rise of AI. Villani frequently collaborates with various artists, including Bailey Ryherd, Omar Ochoa, and Daniel Calderon, to create innovative and thought-provoking works.',
     website: 'https://www.spindletop.digital',
   },
-  // {
-  //   id: '3',
-  //   name: 'Olesia Y. Rodionova',
-  //   picture: 'olesia-1.jpg',
-  //   description:
-  //     'Olesia is a versatile artist working in traditional and digital mediums, including watercolor, algorithmic art, blackwork tattooing, and brand conceptualization. An AI enthusiast focused on ethics, she explores the intersection of technology and identity. Originally from Ukraine, she started her U.S. career in 2023 with &quot;Dreaming. POV: you are an android,&quot; a project depicting an android’s perspective in sleep mode. She has since participated in over ten collective shows at Hardy and Nance Studio, the &quot;Don’t Call Me Sweetie&quot; show by Insomnia Gallery, and was published in the fubar.space 2k23 catalogue with a glitch art fashion project.',
-  // },
+  {
+    id: '3',
+    name: 'Olesia Y. Rodionova',
+    picture: 'olesia-1.jpg',
+    description:
+      'Olesia is a versatile artist working in traditional and digital mediums, including watercolor, algorithmic art, blackwork tattooing, and brand conceptualization. An AI enthusiast focused on ethics, she explores the intersection of technology and identity. Originally from Ukraine, she started her U.S. career in 2023 with &quot;Dreaming. POV: you are an android,&quot; a project depicting an android’s perspective in sleep mode. She has since participated in over ten collective shows at Hardy and Nance Studio, the &quot;Don’t Call Me Sweetie&quot; show by Insomnia Gallery, and was published in the fubar.space 2k23 catalogue with a glitch art fashion project.',
+  },
 ];
 
 export default artists;
