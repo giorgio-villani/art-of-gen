24e16959248afb54635d96342fd75cdf4c5877ff
giorgio-villani <giorgio-villani@users.noreply.github.com>
2024-10-30 20:44:19 -0500

    updated pieces section

--- src/app/pieces/page.tsx
+++ src/app/pieces/page.tsx
@@ -3,15 +3,22 @@ import pieces from '@/data/pieces';
 
 export default function Artists() {
   return (
-    <div className="w-full">
-      <h1 className="text-5xl text-center font-inter pb-5">Pieces</h1>
-      {pieces.map((piece, index) => (
-        <div key={piece.id} className="bg-white py-5 lg:py-10 text-center text-2xl">
-          <a className="hover:underline" href={`/pieces/${piece.id}`}>
-            {piece.name}
-          </a>
-        </div>
-      ))}
+    <div className="w-full max-w-5xl mx-auto">
+      <h1 className="text-5xl text-center font-inter pb-5">Gallery</h1>
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 m-2">
+        {pieces.map((piece) => (
+          <div key={piece.id} className="bg-gray-300 relative overflow-hidden flex items-center justify-center text-2xl text-center text-white p-2">
+            <a href={`/pieces/${piece.id}`}>
+              <div className="absolute inset-0 flex items-center justify-center">
+                {/* <span>{piece.name}</span> */}
+                {/* <img src="" alt="" /> */}
+                <img src={`/pieces/${piece.picture}`} alt={piece.name} />
+              </div>
+            </a>
+            <div className="pb-[100%]"></div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
